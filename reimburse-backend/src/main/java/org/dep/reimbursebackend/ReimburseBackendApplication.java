29e05c2b4405ed521bdf026d0734d41cd3c7051f
yzj_luf <3162794813@qq.com>
2026-05-22 14:33:13 +0800

    feat: 后端初始化

---
+++ reimburse-backend/src/main/java/org/dep/reimbursebackend/ReimburseBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dep.reimbursebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimburseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReimburseBackendApplication.class, args);
+    }
+
+}
---
+++ reimburse-backend/src/test/java/org/dep/reimbursebackend/ReimburseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dep.reimbursebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimburseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
